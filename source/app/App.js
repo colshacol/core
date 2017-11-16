e1bd22142016907f0cfd7d1ee7b6120b4713cadf
Colton Colcleasure <colton.colcleasure.ctr@sabre.com>
2017-11-15 19:39:53 -0600

    satisfies #2

---
+++ source/app/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './styles/App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<h1 styleName='title'>Hahahah</h1>
+		)
+	}
+}
+
+export default App;
--- source/app/index.js
+++ source/app/index.js
@@ -1,3 +1,25 @@
 // NOTE: Directory for main pop-out application.
-
+import '@babel/polyfill';
 import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './App';
+import './styles/index.css';
+
+const render = (Component) => {
+	ReactDOM.render(
+		<AppContainer>
+			<App/>
+		</AppContainer>,
+		document.getElementById('root')
+	);
+}
+
+render(App);
+
+module.hot && module.hot.accept('./App', () => {
+	render(App);
+})
+
+
